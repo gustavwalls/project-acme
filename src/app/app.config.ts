ec265d9c3e8347da3e1372fc376226c5bbafe551
gustavwalls <paredes.ga0610@gmail.com>
2025-01-12 13:18:11 -0500

    update es

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,18 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { getSpanishPaginatorIntl } from './custom/custom-mat-paginator-intl';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { CUSTOM_DATE_FORMATS } from './custom/date-adapter';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimationsAsync(),
-    provideHttpClient()
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),provideAnimationsAsync(),
+    provideHttpClient(),
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
+    { provide: MatPaginatorIntl, useValue: getSpanishPaginatorIntl() },
+    { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS }
   ]
 };
