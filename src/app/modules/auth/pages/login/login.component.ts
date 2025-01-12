fe9fdd76d3db91c5b94fca76029d5d06cd80dfe7
gustavwalls <paredes.ga0610@gmail.com>
2025-01-12 12:54:58 -0500

    update email

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent {
 
   constructor(private fb: FormBuilder, private router: Router ,private cookie: CookieService) {
     this.loginForm = this.fb.group({
-      email: ['',[Validators.required]],
+      email: ['',[Validators.required,Validators.email]],
       password: ['',[Validators.required]]
     });
   }
